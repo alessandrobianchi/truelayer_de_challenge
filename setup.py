eadb4a74229abc06e8f702e84bd4449be21e020d
a.bianchi <alessandro.bianchi231@gmail.com>
2021-12-09 15:27:59 +0100

    Cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Engineer Challenge v1.04 from TrueLayer',
+    author='Alessandro Bianchi',
+    license='',
+)
